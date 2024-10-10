369faecc7f7abee0afd14935feb675033ad0ba4f
snaqvi <snaqvi@taltech.com>
2024-10-10 18:38:30 +0300

    adding users

--- lib/takso_web/controllers/user_controller.ex
+++ lib/takso_web/controllers/user_controller.ex
@@ -7,4 +7,20 @@ defmodule TaksoWeb.UserController do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
   end
+
+
+  #new
+  def new(conn, _params) do
+    changeset = User.changeset(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  #create
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    Repo.insert(changeset)
+    redirect(conn, to: ~p"/users")
+  end
+
 end
