8cd5f4a092fd8f3dde085081f7790d0ee8ea0a5f
snaqvi <snaqvi@taltech.com>
2024-10-19 13:27:26 +0300

    test controller fails

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -4,4 +4,19 @@ defmodule TaksoWeb.BookingController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+  def create(conn, %{"booking" => booking_params}) do
+    pickup_address = booking_params["pickup_address"]
+    dropoff_address = booking_params["dropoff_address"]
+
+    # Mock logic for calculating taxi arrival time (for now, it can be a fixed value or a random one)
+    arrival_time = Enum.random(5..15)
+
+    # Put a flash message with the taxi arrival information
+    conn
+      |> put_flash(:info, "Your taxi will arrive in #{arrival_time} minutes")
+      |> redirect(to: Routes.booking_path(conn, :index))
+  end
 end
--- lib/takso_web/controllers/booking_html.ex
+++ lib/takso_web/controllers/booking_html.ex
@@ -2,4 +2,5 @@ defmodule TaksoWeb.BookingHTML do
   use TaksoWeb, :html
 
   embed_templates "booking_html/*"
+
 end
